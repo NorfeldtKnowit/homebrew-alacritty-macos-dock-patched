4e7c69824640e60d6ed25d9062c8aa09dc6102fb
Lasse Nørfeldt, Knowit <lasse.norfeldt@knowit.dk>
2025-12-16 13:34:57 +0100

    Initial tap with macOS dock menu patch

---
+++ Formula/alacritty-patched.rb
@@ -0,0 +1,35 @@
+class AlacrittyPatched < Formula
+  desc "GPU-accelerated terminal emulator (with macOS dock menu)"
+  homepage "https://github.com/alacritty/alacritty"
+  url "https://github.com/alacritty/alacritty/archive/refs/tags/v0.16.1.tar.gz"
+  sha256 "b7240df4a52c004470977237a276185fc97395d59319480d67cad3c4347f395e"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+  depends_on "scdoc" => :build
+
+  # Apply patch from tap root
+  def patches
+    "#{tap.path}/alacritty-dock-menu.patch"
+  end
+
+  def install
+    system "make", "app"
+    prefix.install "target/release/osx/Alacritty.app"
+    bin.install_symlink prefix/"Alacritty.app/Contents/MacOS/alacritty"
+  end
+
+  def caveats
+    <<~EOS
+      To use as GUI app, create symlink:
+        ln -s #{prefix}/Alacritty.app /Applications/Alacritty-Patched.app
+
+      Or launch with:
+        open #{prefix}/Alacritty.app
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/alacritty --version")
+  end
+end
